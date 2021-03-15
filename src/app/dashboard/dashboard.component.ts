9f43641d13647f6b3b31b004c5801b9a72490b6f
Ranjan Panda <ranjan.net14@gmail.com>
2021-03-15 12:20:26 +0530

    Observable Examples

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
+// import { Observable, Subscription } from 'rxjs-compat';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -30,7 +33,7 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
     
   `]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -52,5 +55,100 @@ export class DashboardComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
+  // numSubs:Subscription;
+  obSubs: Subscription;
+  ngOnInit(): void {
+    // let numObs = Observable.interval(1000);
+
+    // this.numSubs = numObs.subscribe((num) => {
+    //   console.log(num);
+    // });
+
+    // let obs = new Observable((o: Observer<string>) => {
+    //   setTimeout(() => {
+    //     o.next("First data");
+    //   }, 1000);
+
+    //   setTimeout(() => {
+    //     o.next("Second data");
+    //   }, 3000);
+
+    //   setTimeout(() => {
+    //     o.next("Third data");
+    //   }, 5000);
+
+    //   // setTimeout(() => {
+    //   //   o.error("Error Occurred");
+    //   // }, 6000);
+
+    //   setTimeout(() => {
+    //     o.complete();
+    //   }, 6000);
+
+    //   setTimeout(() => {
+    //     o.next("Forth Data");
+    //   }, 7000);
+    // });
+
+
+    // this.obSubs = obs.subscribe(
+    //   (data) => { console.log(data); },
+    //   (error) => { console.log("Some error occurred in the Observable"); },
+    //   () => { console.log("Completed"); }
+    // );
+
+    // let subjectObs = new Subject();
+    // setTimeout(() => {
+    //   subjectObs.next("First data");
+    // }, 1000);
+
+    // setTimeout(() => {
+    //   subjectObs.next("Second data");
+    // }, 3000);
+
+    // setTimeout(() => {
+    //   subjectObs.next("Third data");
+    // }, 5000);
+
+    // setTimeout(() => {
+    //   subjectObs.error("Error Occurred");
+    // }, 6000);
+
+    // setTimeout(() => {
+    //   subjectObs.next("Forth Data");
+    // }, 7000);
+
+
+    // subjectObs.subscribe((data) => {
+    //   console.log(data);
+    // }, (error) => {
+    //   console.log(error);
+    // });
+
+
+    let eventEm: EventEmitter<string> = new EventEmitter();
+    setTimeout(() => {
+      eventEm.emit("First data from Event Emitter");
+    }, 1000);
+    setTimeout(() => {
+      eventEm.emit("Second data from Event Emitter");
+    }, 3000);
+    setTimeout(() => {
+      eventEm.emit("Third data from Event Emitter");
+    }, 5000);
+    setTimeout(() => {
+      eventEm.error("Error Occurred");
+    }, 6000);
+    setTimeout(() => {
+      eventEm.emit("First data from Event Emitter");
+    }, 7000);
+
+    eventEm.subscribe((data) => { console.log(data) }, (e) => { console.warn(e) });
+  }
+
+  ngOnDestroy(): void {
+    // this.numSubs.unsubscribe();
+    // this.obSubs.unsubscribe();
+  }
 }
